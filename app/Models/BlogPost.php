558cef2130e48c24228a38116630324ea6527623
GabrielZapataSarmiento <gabriel.zapatasarmiento@student.kdg.be>
2024-03-19 16:06:41 +0100

    Update BlogPost.php
    comments functie die alle comments ophaald op basisc van blogid
    
    deletefunctie die opbasis van id de blog soft delete en dan terug redirect naar de vorige pagina

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -2,15 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
     protected $table = 'blogposts';
+    use HasFactory;
+    use SoftDeletes;
 
     public static function getBlogs()
     {
         return static::whereNull('deleted_at')->get();
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comments::class, 'blog_id');
+    }
+
+    public function deleteBlog($id){
+        $blogPost = BlogPost::findOrFail($id);
+
+        $blogPost->delete();
+
+        return redirect()->back();
+    }
+
 }
 
