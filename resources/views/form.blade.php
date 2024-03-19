f48bb988545b76e900090f51b1471cb87fe7a9a5
GabrielZapataSarmiento <gabriel.zapatasarmiento@student.kdg.be>
2024-03-19 13:36:04 +0100

    Create form.blade.php
    tailwind html met een extrends voor header @section('content) is dan verder van yield('content)

---
+++ resources/views/form.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.header')
+
+@section('content')
+    <div class="max-w-7xl mx-auto">
+        <form action="{{ $form_action }}" method="post" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            @csrf
+            <div class="mb-6">
+                <label for="title" class="block text-gray-700 text-sm font-bold mb-2">Title</label>
+                <input type="text" id="title" name="title" placeholder="Enter the title of your blog post" class="shadow appearance-none border rounded w-full py-3 px-4 text-lg text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+            </div>
+            <div class="mb-6">
+                <label for="content" class="block text-gray-700 text-sm font-bold mb-2">Content</label>
+                <textarea id="content" name="content" placeholder="Enter the content of your blog post" class="shadow appearance-none border rounded w-full h-48 py-3 px-4 text-lg text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
+            </div>
+            <div class="mb-6">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full focus:outline-none focus:shadow-outline w-full">
+                    {{ $form_button }}</button>
+            </div>
+            <p class="text-center text-gray-500 text-xs">
+            </p>
+        </form>
+    </div>
+
+@endsection
