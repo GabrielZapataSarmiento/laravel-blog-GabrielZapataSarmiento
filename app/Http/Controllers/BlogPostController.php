3bf757ec0d4b60de728c1dc5785b33894fe87b33
GabrielZapataSarmiento <gabriel.zapatasarmiento@student.kdg.be>
2024-03-24 18:55:18 +0100

    Update BlogPostController.php
    pagiantion toegevoegd niewste blog wordt als eerste laaten zien bij een blog horen ook likes dus die geven we mee aan de getoneblog functie ook een functie om al uw eigenblogs te zien ook met pagination en weer de msg die wij latne zien als een niet ingeloggde user die actie probeert uittevoeren

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -4,35 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\BlogPost;
 use App\Models\Comments;
+use App\Models\Likes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BlogPostController extends Controller
 {
     public function index()
     {
-        $blogs = BlogPost::getBlogs();
+        $blogs = BlogPost::orderBy('created_at', 'desc')->paginate(5);
+
         return view('index', compact('blogs'));
     }
+
     public function getOneBlog($id)
     {
         $blogPost = BlogPost::findOrFail($id);
         $comments = Comments::getComments($id);
+        $likes = Likes::getLikes($id);
 
-        return view('view', compact('blogPost', 'comments'));
+        return view('view', compact('blogPost', 'comments', 'likes'));
     }
-
-    public function store(Request $request)
+    public function myBlogs()
     {
-        $validatedData = $request->validate([
-            'title' => 'required|max:255',
-            'content' => 'required',
-        ]);
+        if (Auth::check()) {
+            $user = Auth::user();
 
-        $blog = new BlogPost;
-        $blog->title = $validatedData['title'];
-        $blog->content = $validatedData['content'];
-        $blog->save();
+            $blogs = BlogPost::where('user_id', $user->id)->orderBy('created_at', 'desc')->paginate(5);
 
-        return redirect()->route('blogs.index');
+            return view('index', compact('blogs'));
+        } else {
+            return redirect()->route('login')->with('msg', 'You need to be logged in to do this');
+        }
     }
+
 }
