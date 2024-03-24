c0b65f94ec3ad35af829469694b4ddd07375c2fa
GabrielZapataSarmiento <gabriel.zapatasarmiento@student.kdg.be>
2024-03-24 19:00:01 +0100

    Update Comments.php
    functie delete comments maken gebruik van softdeletes en we zeggen dat een aantal dingens required ztijn comment, user en blog_id dan check we of de comments userid hetzelfde is als de loggedinuserid en de comment blogid hetzlegde is als de gegeven blog id zo niet dan redirecten we terug anders delete we de comment

--- app/Models/Comments.php
+++ app/Models/Comments.php
@@ -4,13 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Comments extends Model
 {
+    protected $table = 'comments';
+    use HasFactory, SoftDeletes;
+
     public static function getComments($blogPostId)
     {
         return static::where('blog_id', $blogPostId)
             ->whereNull('deleted_at')
             ->get();
     }
+
+    public function deleteComment(Request $request)
+    {
+        $request->validate([
+            'comment_id' => 'required|exists:comments,id',
+            'user_id' => 'required|exists:users,id',
+            'blog_id' => 'required|exists:blogposts,id',
+        ]);
+
+        $comment = Comments::findOrFail($request->input('comment_id', 'blog_id'));
+        if ($comment->user_id != Auth::id() || $comment->blog_id != $request->input('blog_id')) {
+            return redirect()->back();
+        }
+
+        $comment->delete();
+
+        return redirect()->back();
+    }
+
+
 }
