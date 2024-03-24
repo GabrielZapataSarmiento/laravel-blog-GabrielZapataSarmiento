603587174903e096b17f343def99429d4c2502e6
GabrielZapataSarmiento <gabriel.zapatasarmiento@student.kdg.be>
2024-03-24 19:10:24 +0100

    Create web.php
    eerste rout is naar de index waar alle blogs worden laten zien 2de is om een blog ze zien dan /home is voor de login bericht van laravel/ui dan maken we een groep voor de rest aan om iets overzichtelijker te zijn dit is voor add edit en delete | edit heeft een {id} nodig om de blogpostid mee integeven omdaarna de content van die blogpost in de form te tonen daarna hebbe we login register en log out van laravel/ui
    
    dan een niewue group voor view dit gebeurt er allemaal als je 1 blog bekijkt dan kan comment adden deleten en like adden worden allemaal verwezen naar hun functies
    
    myblogs om al uw eigen blogs te zien
    
    route naar login en reigster en lgoout

---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\BlogPostController;
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+
+use App\Models\BlogPost;
+use App\Models\Comments;
+
+Auth::routes();
+
+Route::get('/', [BlogPostController::class, 'index'])->name('blogs.index');
+Route::get('/view/{id}', [BlogPostController::class, 'getOneBlog'])->name('blogs.show');
+
+Route::get('/home', function () { return view('home');});
+
+Route::group(['prefix' => 'blog', 'as' => 'blog.'], function () {
+    Route::get('/add', [FormController::class, 'add'])->name('add');
+    Route::post('/store', [FormController::class, 'store'])->name('store');
+
+    Route::get('/edit/{id}', [FormController::class, 'edit'])->name('edit');
+    Route::post('/update/{id}', [FormController::class, 'update'])->name('update');
+
+    Route::get('/delete/{id}', [BlogPost::class, 'deleteBlog'])->name('delete');
+});
+
+
+
+
+Route::get('/login', function () {return view('auth.login');})->name('login');
+
+Route::get('/register', function () {return view('auth.register');})->name('register');
+
+Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
+
+Route::group(['prefix' => '/view', 'as' => 'view.'], function () {
+    Route::post('comment/add/{id}', [CommentController::class, 'add'])->name('comment.add');
+    Route::delete('comment/delete', [Comments::class, 'deleteComment'])->name('comment.delete');
+    Route::post('like/add/{id}', [LikeController::class, 'addLike'])->name('like.add');
+});
+
+Route::get('/myblogs', [BlogPostController::class, 'myBlogs'])->name('myblogs');
+
+
+
+
+
+
