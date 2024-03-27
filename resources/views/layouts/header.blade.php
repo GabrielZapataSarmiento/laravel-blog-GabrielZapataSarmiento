875c11f23a847a1d1c2511ec896342a5a61a2bc6
GabrielZapataSarmiento <gabriel.zapatasarmiento@student.kdg.be>
2024-03-27 18:29:15 +0100

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -15,10 +15,7 @@
     <header class="bg-white">
         <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div class="flex lg:flex-1">
-                <a href="#" class="-m-1.5 p-1.5">
-                    <span class="sr-only">Your Company</span>
-                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
-                </a>
+
             </div>
             <div class="hidden lg:flex lg:gap-x-12">
                 <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Home</a>
